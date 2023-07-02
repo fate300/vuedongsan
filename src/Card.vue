47193bcebb3fa9098fc3292f4c99007bd774d8d6
강지행 <jey78v@naver.com>
2023-07-02 14:07:10 +0900

    main_vue

---
+++ src/Card.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+  <img :src="원룸.image" class="room-img">
+  <!-- <h4 @click="모달창열렸니=true; 누른거 = i">{{원룸들[i].title}}</h4> -->
+  <h4 @click="$emit('openModal', 원룸.id )">{{원룸.title}}</h4>
+  <p>{{원룸.price}}원</p>
+  </div>
+  </template>
+  
+  <script>
+  export default {
+      name:'Card-page',
+   props: {
+   원룸: Object,
+  //  모달창열렸니:Boolean,
+  //  누른거:Number,
+   }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
---
+++ src/Modal.vue
@@ -0,0 +1,49 @@
+<template>
+   <div class="black-bg" v-if="모달창열렸니 == true"> 
+  <div class="white-bg">
+  <img :src="원룸들[누른거].image" class="room-img">
+  <h4>{{원룸들[누른거].title}} </h4>
+  <p>{{원룸들[누른거].content}}원</p>
+  <input v-model.number="month">개월
+  <!-- <input type = "range" min="1" max="12"> -->
+  <p>{{원룸들[누른거].price*month}}원</p>
+  <!-- <button @click="모달창열렸니=false">닫기</button> -->
+  <button @click="$emit('closeModal')">닫기</button>
+
+  <Discount/>  
+</div>
+  
+  </div> 
+</template>
+
+<script>
+export default {
+data(){
+return {
+month:1
+
+}
+},
+watch :{
+ month(a){
+if(a >= 13 ){ 
+alert('13이상 입력하지 마셈')
+ this.month =1 
+} else if (isNaN(a) == true ){
+  alert('문자를 입력하면 안됩니다.')
+  this.month=1 
+}
+},
+},
+name:'Modal-page',
+props: {
+원룸들: Array,
+누른거: Number,
+모달창열렸니:Boolean, 
+}
+}
+</script>
+
+<style>
+
+</style>
